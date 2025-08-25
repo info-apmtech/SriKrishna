adb232ef82dc32ee53a190eb2431861c38986fe5
harini232 <harini.renganathan02@gmail.com>
2025-08-25 15:27:04 +0530

    prj inti

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SriKrishna.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
